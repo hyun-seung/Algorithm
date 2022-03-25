7f4f8aa512a2c50922df727dd5a95a03eaad03ab
hyun-seung <78414370+hyun-seung@users.noreply.github.com>
2022-03-25 12:36:57 +0900

    [백준] 연속합_1912

---
+++ src/BAEKJOON/연속합_1912.java
@@ -0,0 +1,41 @@
+package BAEKJOON;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 연속합_1912 {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    static int N;
+    static int[] arr;
+    static int[] dp;
+
+    public static void base() throws IOException {
+        N = Integer.parseInt(br.readLine());
+
+        arr = new int [N+1];
+        dp = new int [N+1];
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for(int i=0; i<N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        base();
+
+        int max = arr[0];
+
+        dp[0] = arr[0];
+        for(int i=1; i<N; i++) {
+            dp[i] = Math.max(dp[i-1] + arr[i], arr[i]);
+
+            max = Math.max(max, dp[i]);
+        }
+
+        System.out.println(max);
+    }
+}
